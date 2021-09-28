833df8fdf976fda7fffbb9dca904ad2b0661bd89
clvasiliu <clvasiliu@berkeley.edu>
2021-09-27 17:16:35 -0700

    Add starter

---
+++ Easy/sum.py
@@ -0,0 +1,12 @@
+class Problem:
+    """
+    This function is meant to return the sum of two inputs passed in.
+    For Example:
+
+    sum(2, 4) should return 6
+    """
+    def sum(input1, input2):
+        """
+        :type int:
+        :type int:
+        """
